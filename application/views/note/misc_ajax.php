3385a855aa55cf6f2597dd1036e5991b5d094d4b
counterall <kcongmj23@gmail.com>
2016-01-09 21:26:06 +0200

    use MainLib class for misc_ajax.php

--- application/views/note/misc_ajax.php
+++ application/views/note/misc_ajax.php
@@ -1,48 +1,47 @@
 <?php
 
-include_once "functions.php";
+function myAutoLoad6($class){
+  include_once "./classes/$class".".php";
+}
+
+spl_autoload_register('myAutoLoad6');
+
+$main = new MainLib();
 
 if (isset($_POST['recentlyUpdated'])) {
-  connectDB();
-  $return_html1 = getRecentUpdateAndMostSearch();
-  $return_html2 = getRecentUpdateAndMostSearch(false);
-  closeDB();
+  $return_html1 = $main->getRecentUpdateAndMostSearch();
+  $return_html2 = $main->getRecentUpdateAndMostSearch(true);
   $return_html = $return_html1 . 'ckseparator' . $return_html2;
   echo $return_html;
 }
 
 if (isset($_POST['updateSubCats'])) {
-  connectDB();
-  $sql = "SELECT DISTINCT(sub_cat) AS c FROM notes WHERE category = '".$_POST['cat']."' ORDER BY c ASC";
-  $results = querySql($sql, true);
-  closeDB();
-  $sub_cats = [];
+  $cat = $main->db->sanitizeQuery($_POST['cat']);
+  $sql = "SELECT DISTINCT(sub_cat) AS c FROM notes WHERE category = '$cat' ORDER BY c ASC";
+  $sub_cats = $main->db->getQuery($sql);
   $subCatsHTML = '';
-  while ($row = $results->fetch_array(MYSQLI_ASSOC)) {
-    $sub_cats[] = $row['c'];
-  }
-  foreach ($sub_cats as $key => $value) {
-    $val = str_replace(' ', '_', $value);
-    $subCatsHTML .= '<option value="'.strtolower($val).'">'.$value.'</option>';
+  foreach ($sub_cats as $value) {
+    $val = str_replace(' ', '_', $value['c']);
+    $subCatsHTML .= '<option value="'.strtolower($val).'">'.$value['c'].'</option>';
   }
   echo $subCatsHTML;
 }
 
 if (isset($_POST['id'])) {
-  connectDB();
-  $id = $_POST['id'];
+  $id = $main->db->sanitizeQuery($_POST['id']);
   if (isset($_POST['visit'])) {
     $sql = "UPDATE notes SET visits = visits + 1 WHERE id = $id";
-    querySql($sql);
+    $main->db->setQuery($sql);
   }
   $sql = "SELECT category, sub_cat, title, content FROM notes WHERE id = $id";
-  $result = querySql($sql, true);
-  closeDB();
-  $row = $result->fetch_array(MYSQLI_ASSOC);
+  $result = $main->db->getQuery($sql);
+  $row = $result[0];
   $data = $row['category']."[separator]";
   $data .= $row['sub_cat']."[separator]";
   $data .= $row['title']."[separator]";
   $data .= $row['content'];
   echo $data;
 }
+
+$main->dbClose();
 ?>
