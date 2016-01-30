0cca8350dcc4eb14c91ec822a5000b0d2956afbe
counterall <kcongmj23@gmail.com>
2016-01-30 11:57:43 +0200

    modify custom style of ckeditor

--- application/views/note/ckeditor_style.js
+++ application/views/note/ckeditor_style.js
@@ -1,9 +1,8 @@
 CKEDITOR.stylesSet.add( 'my_styles', [
-    // Block-level styles
-    { name: 'Red Heading', element: 'p', styles: { 'color': '#B22222' } },
-
-    // Inline styles
-    { name: 'Inline Blue', element: 'span', styles: { 'background-color': '', 'color': '#00A8FF' } },
-    { name: 'Inline Green', element: 'span', styles: { 'background-color': '', 'color': '#52bb5d' } },
-    { name: 'Bold Yellow Bg', element: 'span', styles: { 'background-color': '#e6de22', 'color': 'black'} }
+  // Inline styles
+  { name: 'Red Heading', element: 'span', styles: { 'color': '#B22222' } },
+  { name: 'Blue Bg', element: 'span', styles: { 'background-color': '#17aef0', 'color': 'whitesmoke' } },
+  { name: 'Green Bg', element: 'span', styles: { 'background-color': '#52b15b', 'color': 'whitesmoke' } },
+  { name: 'Orange Bg', element: 'span', styles: { 'background-color': '#eba03d', 'color': 'whitesmoke'} },
+  { name: 'Red Bg', element: 'span', styles: { 'background-color': '#f14c4c', 'color': 'whitesmoke'} }
 ] );
