276efaad242bc805aef29ca93c7f1d25ac5384a3
Kan Cong <kcongmj23@gmail.com>
2016-10-21 19:56:59 +0300

    ckeditor inline style modification

--- application/views/note/ckeditor_style.js
+++ application/views/note/ckeditor_style.js
@@ -1,8 +1,8 @@
 CKEDITOR.stylesSet.add( 'my_styles', [
   // Inline styles
   { name: 'Red Heading', element: 'span', styles: { 'color': '#B22222' } },
-  { name: 'Blue Bg', element: 'span', styles: { 'background-color': '#17aef0', 'color': 'whitesmoke' } },
-  { name: 'Green Bg', element: 'span', styles: { 'background-color': '#52b15b', 'color': 'whitesmoke' } },
-  { name: 'Orange Bg', element: 'span', styles: { 'background-color': '#eba03d', 'color': 'whitesmoke'} },
-  { name: 'Red Bg', element: 'span', styles: { 'background-color': '#f14c4c', 'color': 'whitesmoke'} }
+  { name: 'Green Bg', element: 'span', styles: { 'background-color': '#7AFF91'} },
+  { name: 'Orange Bg', element: 'span', styles: { 'background-color': '#FFEC00'} },
+  { name: 'Red Bg', element: 'span', styles: { 'background-color': '#FF7683'} },
+  { name: 'White Bg', element: 'span', styles: { 'background-color': 'white'} }
 ] );
